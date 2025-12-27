25b8749422cdbaf0d04d488b797a5c92fe75d91d
sepideh asgharpoor hooshiar <sepideh.a.hooshiar@gmail.com>
2025-12-27 22:10:12 +0500

    colours and fruit images are added.

---
+++ FructusApp/FructusApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FructusApp
+//
+//  Created by sepideh aph on 2025-12-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FructusApp/FructusApp/FructusAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusAppApp.swift
+//  FructusApp
+//
+//  Created by sepideh aph on 2025-12-27.
+//
+
+import SwiftUI
+
+@main
+struct FructusAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
